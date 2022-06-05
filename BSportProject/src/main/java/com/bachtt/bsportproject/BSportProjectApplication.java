000a8b1b0ad14868494a6f8fbdbfaea576c92cf8
Bachtt <Bachtt107@gmail.com>
2022-06-05 16:23:15 +0700

    initialize project and add dependency

---
+++ BSportProject/src/main/java/com/bachtt/bsportproject/BSportProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bachtt.bsportproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BSportProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BSportProjectApplication.class, args);
+    }
+
+}
